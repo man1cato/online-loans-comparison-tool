ff3c485ff2ed8115b92031f6d5d4a14d682786c5
man1cato <andresrod21@gmail.com>
2018-08-27 15:28:12 -0400

    Server fix for heroku

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 8080;
 
 app.use(express.static(publicPath));
+app.use(bodyParser.json());
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 })
 
-app.listen(3000, () => {
-    console.log('Server is up!')
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
 });
