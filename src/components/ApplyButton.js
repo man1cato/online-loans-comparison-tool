6c7690ffc90028d937fecf0dfeda1f0d99623979
man1cato <andresrod21@gmail.com>
2018-10-30 14:21:21 -0400

    Style tweaks for iframe

--- src/components/ApplyButton.js
+++ src/components/ApplyButton.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ApplyButton = ({loan}) => (
     <div className="loan__button">
         <div className="loan__lender-logo"><img src={loan.logo} alt={loan.lender} /></div>
-        <a className="btn btn-secondary" role="button" href={loan.ctaLink}>Apply Now</a>
+        <a className="btn btn-secondary" role="button" href={loan.ctaLink} target="_blank">Apply Now</a>
     </div>
 )
 
