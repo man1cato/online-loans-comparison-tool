daae924f089df7f4ff049f9f67f048fc76a7bb22
man1cato <andresrod21@gmail.com>
2018-10-30 13:57:10 -0400

    Style tweaks

--- src/components/ApplyButton.js
+++ src/components/ApplyButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ApplyButton = ({loan}) => (
-    <div>
+    <div className="loan__button">
         <div className="loan__lender-logo"><img src={loan.logo} alt={loan.lender} /></div>
         <a className="btn btn-secondary" role="button" href={loan.ctaLink}>Apply Now</a>
     </div>
